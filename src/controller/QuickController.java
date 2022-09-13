abd392a65b46f2afeaaad1c3aca2961cbd52ed8b
sarafelicx <111999452+sarafelicx@users.noreply.github.com>
2022-09-13 17:05:34 -0300

    Exercício 4 - Quick_Sort

--- src/controller/QuickController.java
+++ src/controller/QuickController.java
@@ -1,25 +1,62 @@
 package controller;
 
-import br.edu.fateczl.QuickSort.QuickSortController;
-
 public class QuickController {
 
 	public QuickController() {
+
 		super();
 	}
-	
-	public void quick(int [] vetor) {
-		
-		QuickSortController quick = new QuickSortController();
-		
-			
-		int [] vetorOrdenado = quick.quickSort(vetor, 0, vetor.length-1); 
-		
-		for(int valor : vetorOrdenado) {
-			
-			System.out.print(valor);
-			System.out.print(" ");
+
+	public int[] quickSort(int[] vetor, int inicio, int fim) {
+
+		if (fim > inicio) {
+
+			int posicaoFixa = dividir(vetor, inicio, fim);
+			quickSort(vetor, inicio, posicaoFixa - 1);
+			quickSort(vetor, posicaoFixa + 1, fim);
 		}
+
+		return vetor;
+	}
+
+	public int dividir(int[] vetor, int inicio, int fim) {
+
+		int pivo = vetor[inicio];
+		int ponteiroEsquerda = inicio + 1;
+		int ponteiroDireita = fim;
+
+		while (ponteiroEsquerda <= ponteiroDireita) {
+
+			while (ponteiroEsquerda <= ponteiroDireita && vetor[ponteiroEsquerda] <= pivo) {
+
+				ponteiroEsquerda++;
+			}
+
+			while (ponteiroEsquerda <= ponteiroDireita && vetor[ponteiroDireita] >= pivo) {
+
+				ponteiroDireita--;
+			}
+
+			if (ponteiroEsquerda < ponteiroDireita) {
+
+				trocar(vetor, ponteiroEsquerda, ponteiroDireita);
+
+				ponteiroEsquerda++;
+				ponteiroDireita--;
+			}
+		}
+
+		trocar(vetor, inicio, ponteiroDireita);
+		return ponteiroDireita;
+
+	}
+
+	public void trocar(int[] vetor, int i, int j) {
+
+		int auxiliar = vetor[i];
+		vetor[i] = vetor[j];
+		vetor[j] = auxiliar;
+
 	}
 
 }
--- src/view/Main.java
+++ src/view/Main.java
@@ -6,11 +6,20 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		int[] vetor = { 69, 68, 32, 36, 91, 19, 28, 44, 88, 73, 37, 99, 1 };
+		int inicio = 0;
+		int fim = vetor.length - 1;
+
 		QuickController quick = new QuickController();
 
-		int[] vetor = { 89, 54, 18, 71, 93, 45, 19, 14, 99, 2, 27, 3, 58, 91, 75 };
+		vetor = quick.quickSort(vetor, inicio, fim);
 
-		quick.quick(vetor);
+		for (int valor : vetor) {
+
+			System.out.print(valor);
+			System.out.print(" ");
+
+		}
 
 	}
 }
